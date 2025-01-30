cffc165718aa8575cfc846d7ea222aef05070493
Puoch Mabor Makuei <136756243+puoch1of1@users.noreply.github.com>
2025-01-30 10:44:04 +0300

    Add files via upload

---
+++ JAVASCRIPT/document.js
@@ -0,0 +1,11 @@
+// variables
+
+const name = 'John';
+const age = 28;
+const isCool = false;
+const rating = 4.5;
+const x = null;
+const y = undefined;
+let z;
+
+console.log(name);
---
+++ JAVASCRIPT/learning.js
@@ -0,0 +1,5 @@
+function onClick(){
+    document = document.getElementById('btn').innerHTML = 'HELLO JAVASCRIPT';
+}
+
+onClick();
